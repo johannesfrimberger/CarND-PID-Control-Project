fe323e7407fcce677c36d4abe2ee894b2f82e2aa
johannesfrimberger <johannes.frimberger@gmail.com>
2017-05-06 10:47:22 +0200

    First working version of PID controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,27 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(const double Kp_, const double Ki_, const double Kd_)
+{
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
+    d_error = 0;
+    p_error = 0;
+    i_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(const double cte)
+{
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError() const
+{
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
-
