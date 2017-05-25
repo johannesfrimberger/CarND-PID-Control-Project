f77fbd6f87f93e3462e797fac9a99176ef2bd7bd
johannesfrimberger <johannes.frimberger@gmail.com>
2017-05-25 08:36:02 +0200

    Revert "Merge remote-tracking branch 'udacity/master'"
    This reverts commit 4f1ff4029b2d9702b2f2598541f37279762e9a84, reversing
    changes made to 42087a1270af18a1cefc2b6be5d0ca2d90ed1e76.

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,6 @@ T saturate(T val, T min, T max) {
 
 int main()
 {
-<<<<<<< HEAD
     uWS::Hub h;
     
     PID pid(true);
@@ -105,45 +104,6 @@ int main()
         if (req.getUrl().valueLength == 1)
         {
             res->end(s.data(), s.length());
-=======
-  uWS::Hub h;
-
-  PID pid;
-  // TODO: Initialize the pid variable.
-
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-      auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
-        auto j = json::parse(s);
-        std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
->>>>>>> udacity/master
         }
         else
         {
